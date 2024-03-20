9cfd585729e089bfab0a6dfd9c677e99b6a60c6e
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2024-03-20 16:23:01 +0000

    Add CLI test traceback printer

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,41 +1,46 @@
-import os
 import logging
+import os
+import traceback
 
 import pytest
 from click.testing import CliRunner
-
 from osmox import cli
 
 logging.basicConfig(level=logging.INFO)
+
 
 @pytest.fixture
 def fixtures_root():
     return os.path.abspath(os.path.join(os.path.dirname(__file__), "fixtures"))
 
+
 @pytest.fixture
 def config_path(fixtures_root):
     return os.path.join(fixtures_root, "test_config.json")
+
 
 @pytest.fixture
 def toy_osm_path(fixtures_root):
     return os.path.join(fixtures_root, "park.osm")
 
+
 @pytest.fixture
 def runner():
     return CliRunner()
 
+
+def check_exit_code(result):
+    "Print full traceback if the CLI runner failed"
+    if result.exit_code != 0:
+        traceback.print_tb(result.exc_info[-1])
+    assert result.exit_code == 0
+
+
 def test_cli_with_default_args(runner, config_path, toy_osm_path):
     # Test the command with minimal arguments
-    result = runner.invoke(
-        cli.run,
-        [
-            config_path,
-            toy_osm_path,
-            'output_test'
-        ]
-    )
-    #print(result.output)
-    assert result.exit_code == 0
+    result = runner.invoke(cli.run, [config_path, toy_osm_path, "output_test"])
+
+    check_exit_code(result)
     assert "geopackage" in result.exit_code
     assert "epsg:4326" in result.exit_code
 
@@ -44,13 +49,6 @@ def test_cli_output_formats(runner, config_path, toy_osm_path):
     for output_format in ["geojson", "geopackage", "geoparquet"]:
         result = runner.invoke(
             cli.run,
-            [
-                config_path,
-                toy_osm_path,
-                'output_test',
-                "-f", output_format,
-                "-crs", "epsg:4326"
-            ]
+            [config_path, toy_osm_path, "output_test", "-f", output_format, "-crs", "epsg:4326"],
         )
-        print(result.output)
-        assert result.exit_code == 0
+        check_exit_code(result)
